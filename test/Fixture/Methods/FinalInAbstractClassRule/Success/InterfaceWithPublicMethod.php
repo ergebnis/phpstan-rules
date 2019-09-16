225dedd74bc3bdb1e2d2a89533ee0b4f30882d6e
Andreas Möller <am@localheinz.com>
2019-09-16 08:29:58 +0200

    Enhancement: Assert that an error is not reported for an interface with a public method

---
+++ test/Fixture/Methods/FinalInAbstractClassRule/Success/InterfaceWithPublicMethod.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Localheinz\PHPStan\Rules\Test\Fixture\Methods\FinalInAbstractClassRule\Success;
+
+interface InterfaceWithPublicMethod
+{
+    public function method(): void;
+}
