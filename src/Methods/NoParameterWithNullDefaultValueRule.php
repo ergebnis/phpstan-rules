2b1d14fd8033c0ef6c735e9144e7462a312b457c
Andreas Möller <am@localheinz.com>
2024-12-13 09:41:18 +0100

    Enhancement: Extract method

--- src/Methods/NoParameterWithNullDefaultValueRule.php
+++ src/Methods/NoParameterWithNullDefaultValueRule.php
@@ -38,11 +38,7 @@ final class NoParameterWithNullDefaultValueRule implements Rules\Rule
         }
 
         $params = \array_filter($node->params, static function (Node\Param $node): bool {
-            if (!$node->default instanceof Node\Expr\ConstFetch) {
-                return false;
-            }
-
-            return 'null' === $node->default->name->toLowerString();
+            return self::hasNullDefaultValue($node);
         });
 
         if (0 === \count($params)) {
@@ -94,5 +90,14 @@ final class NoParameterWithNullDefaultValueRule implements Rules\Rule
                 ->identifier(ErrorIdentifier::noParameterWithNullDefaultValue()->toString())
                 ->build();
         }, $params));
+    }
+
+    private static function hasNullDefaultValue(Node\Param $node): bool
+    {
+        if (!$node->default instanceof Node\Expr\ConstFetch) {
+            return false;
+        }
+
+        return 'null' === $node->default->name->toLowerString();
     }
 }
