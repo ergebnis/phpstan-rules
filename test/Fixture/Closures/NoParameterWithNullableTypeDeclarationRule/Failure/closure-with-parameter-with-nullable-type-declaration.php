4c54a410406c630b28aa16d0d7b7632a20fc0002
Andreas Möller <am@localheinz.com>
2024-12-13 11:42:14 +0100

    Fix: Namespace

--- test/Fixture/Closures/NoParameterWithNullableTypeDeclarationRule/Failure/closure-with-parameter-with-nullable-type-declaration.php
+++ test/Fixture/Closures/NoParameterWithNullableTypeDeclarationRule/Failure/closure-with-parameter-with-nullable-type-declaration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ergebnis\PHPStan\Rules\Test\Fixture\Closures\NoParameterWithNullDefaultValueRule\Success;
+namespace Ergebnis\PHPStan\Rules\Test\Fixture\Closures\NoParameterWithNullDefaultValueRule\Failure;
 
 $foo = function (?string $bar) {
     return $bar;
