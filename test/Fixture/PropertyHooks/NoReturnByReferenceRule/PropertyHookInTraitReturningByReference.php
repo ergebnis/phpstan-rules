e7db085d6b8d411d05689aa9f9bfcdbb224a979f
Andreas Möller <am@localheinz.com>
2025-02-18 12:18:11 +0100

    Enhancement: Add support for property hooks

---
+++ test/Fixture/PropertyHooks/NoReturnByReferenceRule/PropertyHookInTraitReturningByReference.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ergebnis\PHPStan\Rules\Test\Fixture\PropertyHooks\NoReturnByReferenceRule;
+
+trait PropertyHookInTraitReturningByReference
+{
+    private $foo  {
+        get {
+            return $this->foo;
+        }
+    }
+    private $bar  {
+        &get {
+            return $this->bar;
+        }
+    }
+}
