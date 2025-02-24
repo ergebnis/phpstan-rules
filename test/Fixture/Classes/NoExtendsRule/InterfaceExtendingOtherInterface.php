6d37bb4770f3fb344107c63a5166c12d90a8a00a
Andreas Möller <am@localheinz.com>
2025-02-24 18:35:48 +0100

    Fix: Remove invalid import

--- test/Fixture/Classes/NoExtendsRule/InterfaceExtendingOtherInterface.php
+++ test/Fixture/Classes/NoExtendsRule/InterfaceExtendingOtherInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Ergebnis\PHPStan\Rules\Test\Fixture\Classes\NoExtendsRule;
 
-use Ergebnis\PHPStan\Rules\Test\Fixture\Classes\NoExtendsRule\Success\OtherInterface;
-
 interface InterfaceExtendingOtherInterface extends OtherInterface
 {
 }
